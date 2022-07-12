c23f48c6454944c7f14acb4e997ef1f0fad1503c
KarolisRam <karolis.ram@gmail.com>
2022-07-12 10:19:44 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,8 @@
+# DO NOT MODIFY THIS FILE
+# This is the entry point for your submission.
+# Changing this file will probably fail your submissions.
+
+import test
+
+# Testing Phase
+test.main()
---
+++ test.py
@@ -0,0 +1,45 @@
+import logging
+import coloredlogs
+
+import aicrowd_gym
+import minerl
+
+from config import EVAL_EPISODES, EVAL_MAX_STEPS
+
+coloredlogs.install(logging.DEBUG)
+
+MINERL_GYM_ENV = 'MineRLObtainDiamondShovel-v0'
+
+
+def main():
+    # NOTE: It is important that you use "aicrowd_gym" instead of regular "gym"!
+    #       Otherwise your submission will fail.
+    env = aicrowd_gym.make(MINERL_GYM_ENV)
+
+    # Load your model here
+    # NOTE: The trained parameters must be inside "train" directory!
+    # model = None
+
+    for i in range(EVAL_EPISODES):
+        obs = env.reset()
+        done = False
+        for step_counter in range(EVAL_MAX_STEPS):
+
+            # Step your model here.
+            # Currently, it's doing random actions
+            random_act = env.action_space.sample()
+
+            obs, reward, done, info = env.step(random_act)
+
+            if done:
+                break
+        print(f"[{i}] Episode complete")
+
+    # Close environment and clean up any bigger memory hogs.
+    # Otherwise you might start running into memory issues
+    # on the evaluation server.
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
