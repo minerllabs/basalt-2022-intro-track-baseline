a27e33eb86e37215f80cde86b7b9faac4dead06a
Anssi <kaneran21@hotmail.com>
2022-07-22 17:10:20 +0100

    Sync episode number with aicrowd

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import json
 # These will be enforced in the evaluation server:
 # if you tinker with them, your submissions will almost certainly fail!
 
-# 10 for evaluation + 1 leaderboard video
-EVAL_EPISODES = 11
+# 20 for evaluation + 1 leaderboard video
+EVAL_EPISODES = 21
 # This is only used to limit steps when debugging is on.
 # Environments will automatically return done=True once
 # the environment-specific timeout is reached
